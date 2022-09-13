8612d7c72d0e075cfa036bba613caaf35cde61ac
imaksymovv <Ivan.Vasiliy.Maksymov@gmail.com>
2022-09-13 19:11:45 +0200

    .

--- someproject.cpp
+++ someproject.cpp
@@ -9,27 +9,11 @@ struct answer {
 answer function() {
 	int c = 0;
 	answer r;
-	r.number[0][0] = { 0 };
-	r.number[1][0] = { 0 };
-	r.number[2][0] = { -1 };
 	do {
-		r.number[2][c]++;
-
-		if (r.number[2][c] == 10) {
-			r.number[1][c]++;
-			r.number[2][c] = 0;
-		}
-		if (r.number[1][c] == 10) {
-			r.number[0][c]++;
-			r.number[1][c] = 0;
-		}
-		if (c < 999) {
-			r.number[2][c + 1] = r.number[2][c];
-			r.number[1][c + 1] = r.number[1][c];
-			r.number[0][c + 1] = r.number[0][c];
-		}
+		r.number[2][c] = c % 10;
+		r.number[1][c] = (c % 100) / 10;
+		r.number[0][c] = c / 100;
 		c++;
-
 	} while (c != 1000);
 	return r;
 }
@@ -37,7 +21,7 @@ answer function() {
 int main() {
 	auto p = function();
 	for (size_t i = 0; i < 1000; i++) {
-		std::cout << p.number[0][i] << p.number[1][i] << p.number[2][i] << std::endl;
+		std::cout << p.number[0][i] << p.number[1][i] << p.number[2][i] << " " << i << std::endl;
 	}
 	return 0;
 }
